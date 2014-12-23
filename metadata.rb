e4f9e75cd02ad1417d41a8b2fe145e76c5c057dc
Jonathan Hartman <j@p4nt5.com>
2014-12-22 23:06:19 -0800

    Depend on dmg

--- metadata.rb
+++ metadata.rb
@@ -8,4 +8,9 @@ license          'apache2'
 description      'Installs/Configures private-internet-access'
 long_description 'Installs/Configures private-internet-access'
 version          '0.0.1'
+
+depends          'dmg', '~> 2.2'
+
+supports         'mac_os_x'
+supports         'windows'
 # rubocop:enable SingleSpaceBeforeFirstArg
